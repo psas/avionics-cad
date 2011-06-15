91cea7878291b6c145e585d2a0f098b900d998b9
Andrew Greenberg <jroo@speakeasy.net>
2011-06-14 22:55:04 -0700

    Code review with Glenn. Cleaned up unused code, added comments, changed spacing from holes to board edge, changed wire + rect corners to rect @ 45 degree angle corners, which should hopefully be much better capacitance wise.

--- av3/cpa/cpa-v4.py
+++ av3/cpa/cpa-v4.py
@@ -52,11 +52,12 @@ lambda_0   = c / f                     # [m] Wavelength in free space
 
 airframe_od = 5.5 * 0.0254 # [m] OD of airframe
 
+# For more info on these thicknesses, see http://psas.pdx.edu/CpaDesignv3/
 t_gnd = 0.0050 * 0.0254 # [m] Thickness of GND plane
 t_radiator = 0.0007 * 0.0254 # [m] Thickness of conductor
 t_pcb = 0.0050 * 0.0254 # [m] Thickness of the PCB
 
-h = 0.072 * 0.0254 # [m] Height of dielectric
+h = (0.005 + 0.062 + 0.005) * 0.0254 # [m] Height of dielectric = 3m super 77 adhesive + UHMW PE + acrylic adhesive
 min_padding = 3 * h
 padding = 5 * h
 print("h = " + str(h) + " m so min_padding = " + str(min_padding) + " and padding = " + str(padding))
@@ -67,18 +68,16 @@ l_spacing = 0.36000 # [in] Spacing in diel. Thickness
 
 d_countersink = 0.20 * 0.0254   # [m] #4 nylon screw countersnink hole diameter
 d_screw_hole = 0.089 * 0.0254   # [m] #4-32 FIXME or #4-40?? tap hole
-l_holefromedge = 0.100 * 0.0254 # [m] Dist. From edge to hole
-
-l_paddingtop = 0.300 * 0.0254   # [m] Padding from patch to edge - Makes the screws heads 5h away from the patch slot	
-l_paddingbot = 0.300 * 0.0254   # [m] padding from via to edge - Makes the screws heads 5h away from the via	
-l_paddingwidth = 0.010 * 0.0254 # [m] padding on patch width - Extra slop that we can sand off while making the antenna	
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Cylindrical Patch
 
+# This patch length calculation is from Chapter 7 of The Antenna Engineering Handbook by Richard C. Johnson and Henry Jasik
+# Also see our notes on http://psas.pdx.edu/AntennaDesignLV2/
+
 H = t_gnd + h + t_pcb
 z_0 = sqrt(mu/epsilon) * log(1 + 2 * h / airframe_od) / (2 * pi)
-L  = pi *(airframe_od + H) # [m] FIXME Doesn't include t_pcb since the copper is on the *inside*
+L  = pi *(airframe_od + H) # [m] Note: includes t_pcb, but the copper is on the *inside*. We claim this doesn't make a difference, since we really want to know the length of the FR4.
 BL = L / (120 * pi * lambda_0) * (-0.540754132818691 - 2 * log(f * h / c))
 GL = L / (120 * lambda_0)
 theta = acos((BL * BL + GL * GL-1/(z_0*z_0))/sqrt( pow((BL*BL+GL*GL),2)+1/pow(z_0,4)+2*(BL-GL)*(BL+GL)/(z_0*z_0)))
@@ -87,8 +86,7 @@ patch_length = (theta * lambda_0) / (2 * pi * sqrt(epsilon_r)) # [m] electrical 
 
 print("Patch electrical length = " + str(patch_length) + " m (" + str(patch_length/0.0254) + "in)")
 
-patch_diameter =  airframe_od + 2 * t_gnd + 2 * h # [m] diameter at the patch radiator
-patch_width    = pi * patch_diameter # [m] Width of radiator
+patch_width = pi * (airframe_od + 2 * t_gnd + 2 * h) # [m] Width of radiator
 
 if patch_width > (17 * 0.0254): # 11 x 17 in PCB
 	print('WARNING: patch_width of ' + str(patch_width / 0.0254) + ' inches is wider than 17 in maximum')
@@ -96,8 +94,10 @@ if patch_width > (17 * 0.0254): # 11 x 17 in PCB
 # ---------------------------------------------------------------------------------------------------------------------
 # Calculating feeds and levels. Need at least two feed points.
 
+# Calculate the "fractional" number of feeds given the width of the patch (which is only dependent on the airframe)
 feeds  = patch_width / (lambda_0 / sqrt(epsilon_r))
 
+# Turn the fractional number of feeds into an integer power of 2, necessary for the corporate feed structure.
 number_feeds = 2
 while number_feeds < feeds:
 	number_feeds = number_feeds * 2
@@ -118,8 +118,10 @@ def strip_width(impedance):
 	A = impedance/60.0*sqrt((epsilon_r+1)/2)+(epsilon_r-1)/(epsilon_r+1)*(0.23+0.11/epsilon_r)
 	w_h_ratio2 = 8 * exp(A) / (exp(2 * A ) - 2)
 	if w_h_ratio1 > 2 and w_h_ratio2 > 2:
+		#print("Z = "+str(impedance)+" ohms -> width = "+str(w_h_ratio1*h)+" m")
 		return w_h_ratio1*h
 	else:
+		#print("Z = "+str(impedance)+" ohms -> width = "+str(w_h_ratio2*h)+" m")
 		return w_h_ratio2*h
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -137,8 +139,8 @@ def dprint (label,number):
 # ---------------------------------------------------------------------------------------------------------------------
 # eagle_rect - take coordinates in mm, and write EAGLE script commands to make rectangles in mm
 
-def eagle_rect (x1, y1, x2, y2):
-	script.write('rect (' + str(x1*1000) + ' ' + str(y1*1000) + ') (' + str(x2*1000) + ' ' + str(y2*1000) + ');\n')
+def eagle_rect (x1, y1, x2, y2, rotation = 0):
+	script.write('rect r' + str(rotation) + ' (' + str(x1*1000) + ' ' + str(y1*1000) + ') (' + str(x2*1000) + ' ' + str(y2*1000) + ');\n')
 
 def eagle_wire (width, x1, y1, x2, y2):
 	script.write('wire ' + str(width*1000) +' (' + str(x1*1000) + ' ' + str(y1*1000) + ') (' + str(x2*1000) + ' ' + str(y2*1000) + ');\n')
@@ -229,18 +231,18 @@ def draw_feed(level, feed, y, z_in, z_out, z_next):
 	# If this is the first level, then the Zout microstrip is the most +y thing on the board. Remember it for making holes (see below)
 	if level == 0:
 		feed_top = y + y_width
-
-	# Corner: make a small rectangle to fill the space of the corner
-	eagle_rect(x-x_offset, y, x-x_offset-y_width, y-y_width)
-	eagle_rect(x+x_offset, y, x+x_offset+y_width, y-y_width)
-	# Corner: make the 45 degree piece
-	eagle_wire(y_width, x-x_offset, y+y_width/2, x-x_offset-y_width*1.5, y-y_width)
-	eagle_wire(y_width, x+x_offset, y+y_width/2, x+x_offset+y_width*1.5, y-y_width)
+	
+	# Draw the corner: draw a rectangle and rotate it +/- 45 degrees
+	hypotemoose = sqrt(2)*strip_width(z_out)
+	x_corner =  x-x_offset-strip_width(z_out)/4
+	y_corner =  y-y_width+strip_width(z_out)/4
+	eagle_rect( x_corner-hypotemoose/4, y_corner-hypotemoose/2,  x_corner+hypotemoose/4, y_corner+hypotemoose/2, 315)
+	eagle_rect(-x_corner-hypotemoose/4, y_corner-hypotemoose/2, -x_corner+hypotemoose/4, y_corner+hypotemoose/2, 45)
 
 	# Zout microstrip at 90 degrees
 	x_offset2 = strip_width(z_out)
 	y_offset = -y_width
-	y_offset2 = -(min_padding + strip_width(z_next)/2)
+	y_offset2 = -(padding + strip_width(z_next)/2)
 	eagle_rect(x-x_offset, y+y_offset, x-x_offset-x_offset2, y+y_offset+y_offset2)
 	eagle_rect(x+x_offset, y+y_offset, x+x_offset+x_offset2, y+y_offset+y_offset2)
 
@@ -273,7 +275,7 @@ try:
 		if level == number_levels - 1:
 			z_in  = z_patch/2
 			z_out = z_patch
-			z_next = z_patch
+			z_next = 200 # There's no "next" patch, so make it super thin.
 		elif level == number_levels - 2:
 			if number_levels == 2:
 				z_in  = 50
@@ -358,13 +360,25 @@ try:
 	x_cutting_guide = patch_width/2 + 0.001/2
 	eagle_wire(0.001, x_cutting_guide, feed_top, x_cutting_guide, y + patch_length + min_padding)
 	eagle_wire(0.001, -x_cutting_guide, feed_top, -x_cutting_guide, y + patch_length + min_padding)
-	# Draw outline
-	eagle_wire(0.0002, -x_outer_holes - 0.010, top_row + 0.010, -x_outer_holes - 0.010, bottom_row - 0.01)
-	eagle_wire(0.0002, -x_outer_holes - 0.010, top_row + 0.010, x_outer_holes + 0.010, top_row + 0.010)
-	eagle_wire(0.0002, x_outer_holes + 0.010, top_row + 0.010, x_outer_holes + 0.010, bottom_row - 0.01)
-	eagle_wire(0.0002, -x_outer_holes - 0.010, bottom_row - 0.010, x_outer_holes + 0.010, bottom_row - 0.010)
+	
+	# Make the top and bottom of the antenna PCB are outline_padding away from the top and bottom screws
+	# Make the left and right of the antenna PCB the outer edge of the cutting guide, so we don't waste PCB material
+	# Padding from screw heads to outer edge should be > 1 and < 3 head diameters (according to Tim), so make it 1.5.
 
-	script.write('window fit;\n')  
+	outline_padding = d_countersink*1.5
+	x_outline = x_cutting_guide + 0.001/2 - 0.0002/2
+	top_outline = top_row + outline_padding
+	bottom_outline = bottom_row - outline_padding
+	# Draw the top and bottom outlines
+	eagle_wire(0.0002, -x_outline, top_outline, x_outline, top_outline)
+	eagle_wire(0.0002, -x_outline, top_outline, -x_outline, bottom_outline)
+	eagle_wire(0.0002, -x_outline, bottom_outline, x_outline, bottom_outline)
+	eagle_wire(0.0002,  x_outline, top_outline,  x_outline, bottom_outline)
+
+	final_width = x_outline * 2
+ 	final_length = top_outline - bottom_outline 
+	print('Final dimensions of the antenna are ' + str(final_width*1000) + ' x ' + str(final_length*1000) + ' mm (' + str(final_width/0.0254) + ' x ' + str(final_length/0.0254) + ' in)')
+	script.write('window fit;\n')
 	# -------------------------------------------------------------------------------------------------------------
 	# Script finish
 
