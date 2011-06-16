9b0dc365ac7b4fc9879951d9be4de02503afc65e
Andrew Greenberg <jroo@speakeasy.net>
2011-06-15 22:54:04 -0700

    But officer, I just couldn't help myself! Antennas are now moved to the first quadrant just in case, and copied as many times as will fit in an 11 inch panel. Added flags to not run eagle, and two verbosity levels which I should have done in the first place. OK, we're done here!

--- av3/cpa/cpa-v4.py
+++ av3/cpa/cpa-v4.py
@@ -10,20 +10,43 @@ import subprocess
 from math import sqrt, log, acos, exp, ceil
 
 # =====================================================================================================================
+# Debug printing, with verbosity levels so we only print if we're at the right level of verbosity. Overkill, maybe.
+
+verbosity = 0
+def dprint (v_level, string):
+	if verbosity >= v_level:
+		print(string)
+
+# =====================================================================================================================
 # Parse inputs
 
-if len(sys.argv) < 2:
-	print("Usage: python cpa-v4.py <frequency in GHz>")
+if len(sys.argv) < 2 or len(sys.argv) > 4:
+	print("Usage: ./cpa-v4.py <frequency in GHz> [--no-eagle] [-v|-V]")
         sys.exit(1)
 
+# Parse the one required argument, the frequency in GHz
 f = float(sys.argv[1])
 if f < 0.1 or f > 20:
-	print("frequency must be between 100 MHz and 20 GHz (f = " + str(f) + " GHz)")
+	print("Frequency must be between 0.1 GHz (100 MHz) and 20 GHz (f = " + str(f) + " GHz)")
 	sys.exit(1)
-f = f * 1000000000
-
 filename = 'cpa-v4-' + str(f) + '.scr'
 script = open(filename, 'w')
+dprint(2, "Input frequency = " + str(f) + ' GHz')
+f = f * 1000000000
+
+# Parse other flags, including the "don't launch eagle" flag and the verbose flags
+launch_eagle = True
+for i in range(2,len(sys.argv)):
+	if sys.argv[i] == '--no-eagle':
+		launch_eagle = False
+	elif sys.argv[i] == '-v':
+		verbosity = 1
+	elif sys.argv[i] == '-V':
+		verbosity = 2
+	else:
+		print("Unknown flag. Usage: ./cpa-v4.py <frequency in GHz> [--no-eagle] [-v|-V]")
+	        sys.exit(1)
+	dprint(2, "Other inputs: sys.argv[" + str(i) + '] = ' + str(sys.argv[i]))
 
 # =====================================================================================================================
 # Preliminary calculations 
@@ -40,6 +63,7 @@ mu        = mu_0                       # [] Dielectric permeability
 epsilon   = epsilon_r / (mu_0 * c * c) # [] Dieletric permittivity
 lambda_0   = c / f                     # [m] Wavelength in free space
 
+dprint(2, "epsilon_r = " + str(epsilon_r) + ', lambda_0 = ' + str(lambda_0) + ' m (' + str(lambda_0 / 0.0254) + ' in)')
 # ---------------------------------------------------------------------------------------------------------------------
 # Dimensions
 
@@ -53,7 +77,7 @@ t_pcb = 0.0050 * 0.0254 # [m] Thickness of the PCB
 h = (0.005 + 0.062 + 0.005) * 0.0254 # [m] Height of dielectric = 3m super 77 adhesive + UHMW PE + acrylic adhesive
 min_padding = 3 * h
 padding = 5 * h
-print("h = " + str(h) + " m so min_padding = " + str(min_padding) + " and padding = " + str(padding))
+dprint(1,"h = " + str(h) + " m (" + str(h/0.0254) + "in) -> min_padding = " + str(min_padding) + " and padding = " + str(padding))
 
 d_via = 0.050 * 0.0254 # [m] Diameter of feed via hole (fits Tim's rivet eyelets)
 
@@ -74,15 +98,14 @@ L  = pi *(airframe_od + H) # [m] Note: includes t_pcb, but the copper is on the 
 BL = L / (120 * pi * lambda_0) * (-0.540754132818691 - 2 * log(f * h / c))
 GL = L / (120 * lambda_0)
 theta = acos((BL * BL + GL * GL-1/(z_0*z_0))/sqrt( ((BL*BL+GL*GL) ** 2)+1/(z_0 ** 4)+2*(BL-GL)*(BL+GL)/(z_0*z_0)))
-
 patch_length = (theta * lambda_0) / (2 * pi * sqrt(epsilon_r)) # [m] electrical patch length
-
-print("Patch electrical length = " + str(patch_length) + " m (" + str(patch_length/0.0254) + "in)")
+dprint(1,"Patch electrical length = " + str(patch_length) + " m (" + str(patch_length/0.0254) + "in)")
 
 patch_width = pi * (airframe_od + 2 * t_gnd + 2 * h) # [m] Width of radiator
-
-if patch_width > (17 * 0.0254): # 11 x 17 in PCB
-	print('WARNING: patch_width of ' + str(patch_width / 0.0254) + ' inches is wider than 17 in maximum')
+dprint(1,"Patch width = " + str(patch_width) + " m (" + str(patch_width/0.0254) + "in)")
+if patch_width > (18 * 0.0254): # 11 x 17 in PCB
+	print('Patch_width of ' + str(patch_width / 0.0254) + ' inches is wider than 18 in maximum')
+	sys.exit(1)
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Calculating feeds and levels. Need at least two feed points.
@@ -96,7 +119,7 @@ number_feeds = 2 ** number_levels
 
 z_patch = number_feeds * 60 * lambda_0 / patch_width
 
-print ('Number of feeds = ' + str(number_feeds) + ', number of levels = ' + str(number_levels) + ', Z_patch = ' + str(z_patch) + ' ohms.')
+dprint(1, 'Number of feeds = ' + str(number_feeds) + ', number of levels = ' + str(number_levels) + ', Z_patch = ' + str(z_patch) + ' ohms.')
 
 feed_top = 0 # [m] Top of the feed structure (set below)
 
@@ -109,10 +132,10 @@ def strip_width(impedance):
 	A = impedance/60.0*sqrt((epsilon_r+1)/2)+(epsilon_r-1)/(epsilon_r+1)*(0.23+0.11/epsilon_r)
 	w_h_ratio2 = 8 * exp(A) / (exp(2 * A ) - 2)
 	if w_h_ratio1 > 2 and w_h_ratio2 > 2:
-		#print("Z = "+str(impedance)+" ohms -> width = "+str(w_h_ratio1*h)+" m")
+		dprint(2, "Microstrip: z = "+str(impedance)+" ohms -> width = "+str(w_h_ratio1*h)+" m")
 		return w_h_ratio1*h
 	else:
-		#print("Z = "+str(impedance)+" ohms -> width = "+str(w_h_ratio2*h)+" m")
+		dprint(2, "Microstrip: z = "+str(impedance)+" ohms -> width = "+str(w_h_ratio2*h)+" m")
 		return w_h_ratio2*h
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -122,10 +145,8 @@ def transformer_length(z1,z2):
 	z_transformer = sqrt(z1 * z2)
 	epsilon_effective = (epsilon_r+1)/2+(epsilon_r-1)/2/sqrt(1+12/(h/strip_width(z_transformer)))
 	lambda_effective = lambda_0/sqrt(epsilon_effective)
+	dprint(2, "1/4 wave xfmr: z1/z2/z_eff = " + str(z1) + "/" + str(z2) + "/" + str(z_transformer) +"ohms, length = " + str(lambda_effective/4) + " m")
 	return lambda_effective / 4
-
-def dprint (label,number):
-	print(label + " = " + str(number))
 
 # ---------------------------------------------------------------------------------------------------------------------
 # eagle_rect - take coordinates in mm, and write EAGLE script commands to make rectangles in mm
@@ -153,44 +174,42 @@ def draw_feed(level, feed, y, z_in, z_out, z_next):
 	number_feeds = 2 ** level
 	feed_width = patch_width / (2 * (2 ** level))
 	x = (-1*number_feeds + feed*2 + 1)*feed_width
-	
+	dprint(2, "Feed: Level " + str(level) + " feed " + str(feed) + "/" + str(number_feeds)+ " with y = " + str(y) + " and z_in = " + str(z_in) + " and z_out = " + str(z_out))
+
 	# Calculate how many 1/4 wave transformers we need. If Z_in = Z_out/2, then we don't need any. If that's not
         # the case, then we need at least one transformer. And if we have room, we'd like two to increase bandwidth.
-
 	if z_in == z_out/2:
 		number_transformers = 0
 		strip_length = (feed_width/2 - strip_width(z_out)/2)/2
+		dprint(2,"Feed: No transformers in this feed.")
 	else:
 		number_transformers = int(feed_width / transformer_length(z_in,z_out)) # This is only approximate
 		if number_transformers < 1:
-			print('FATAL: Level ' + str(level) + ' has no room for quarter wave transformers.')
+			print('Feed: Level ' + str(level) + ' has no room for quarter wave transformers.')
 			sys.exit(1)
 		elif number_transformers == 1:
-			print('Warning: Level ' + str(level) + ' has only ' + str(number_transformers) + ' quarter wave transformer in it')
+			print('Feed: Warning: Level ' + str(level) + ' has only ' + str(number_transformers) + ' quarter wave transformer in it')
 			strip_length = (feed_width/2 - (transformer_length(z_in, z_out) + strip_width(z_out)/2))/2
 			if strip_length < min_padding:
-				print('FATAL: Not enough strip for even one quarter wave transformer')
+				print("Feed: Not enough room for even one quarter wave transformer in Level " + str(level))
 				sys.exit(1)
 		else:
 			number_transformers = 2
 			z_intermediate = sqrt((z_in*2) * z_out)
 			strip_length = (feed_width/2 - (transformer_length(z_in, z_intermediate) + transformer_length(z_intermediate, z_out) + strip_width(z_out)/2))/2
 			if strip_length < min_padding:
-				print('Warning: level ' + str(level) + 's strip_length of ' + str(strip_length) + ' m is < min_padding.')
-				print('Warning: moving to one quarter wave transformer')
+				print('Feed: Warning: level ' + str(level) + 's strip_length of ' + str(strip_length) + ' m is < min_padding.')
+				print('Feed: Warning: moving to one quarter wave transformer in Level ' + str(level))
 				number_transformers = 1
 				strip_length = (feed_width/2 - (transformer_length(z_in, z_out) + strip_width(z_out)/2))/2
 
-	# If we're at the number_levels-2 or lower level (nearer to the feedline), then build a standard 50 to 100 ohm feedline with as many q wave 
-	# transformers as will fit.
-
-	# microstrip at Zin from center to first q wave xfrmr
+	# Draw microstrip at Zin from center to either the first q wave xfrmr or to the Zout 90 degree strip
 	x_offset = strip_length
 	y_width = strip_width(z_in*2)/2
 	eagle_rect(x, y-y_width, x-x_offset, y+y_width)
 	eagle_rect(x, y-y_width, x+x_offset, y+y_width)
 
-	# quarter wave transformer(s)
+	# Draw any quarter wave transformer(s)
 	if number_transformers == 1:
 		# Just one q wave xfrmr
 		x_offset2 = transformer_length(z_in*2,z_out)
@@ -212,7 +231,7 @@ def draw_feed(level, feed, y, z_in, z_out, z_next):
 		eagle_rect(x+x_offset, y-y_width, x+x_offset+x_offset2, y+y_width)
 		x_offset += x_offset2
 	
-	# Zout microstrip to 90 degree bend
+	# Draw Zout microstrip to 90 degree bend
 	x_offset2 = strip_length
 	y_width = strip_width(z_out)/2
 	eagle_rect(x-x_offset, y+y_width, x-x_offset-x_offset2, y-y_width)
@@ -225,7 +244,8 @@ def draw_feed(level, feed, y, z_in, z_out, z_next):
 	
 	# Draw the corner: draw a rectangle and rotate it +/- 45 degrees. Note that the 45 degree mitre is close, but not quite the optimal.
 	# We think the optimal is slightly more mitre - see http://en.wikipedia.org/wiki/Microstrip but we're ignoring this because it's hard
-	# to do, and our v3 CPAs were even fatter since they used wires instead of rotated rectangles.
+	# to do and our v3 CPAs (which worked well) were even fatter since they used wires instead of rotated rectangles. Moose!
+
 	hypotemoose = sqrt(2)*strip_width(z_out)
 	x_corner =  x-x_offset-strip_width(z_out)/4
 	y_corner =  y-y_width+strip_width(z_out)/4
@@ -292,10 +312,7 @@ with script:
 		# There are 2^level feed structures on each level (level 0 = 1, level 1 = 2, level 2 = 4, etc)
 		number_feeds = 2 ** level
 		for feed in range(number_feeds):
-			print("starting level " + str(level) + " feed " + str(feed) + " with y = " + str(y) + ' and z_in = ' + str(z_in) + ' and z_out = ' + str(z_out))
 			height = draw_feed(level, feed, y, z_in, z_out, z_next)
-			dprint("feed_top", feed_top)
-
 		y = height
 
 	# Draw the final patch
@@ -307,8 +324,10 @@ with script:
 	bottom_row = y - min_padding - d_countersink/2
 	if ((top_row - bottom_row)/2 + bottom_row) > y + patch_length:
 		middle_row = (top_row - bottom_row)/2 + bottom_row
+		dprint(2,"Holes: Outer middle holes are centered in PCB (not patch).")
 	else:
 		middle_row = y + patch_length/2
+		dprint(2,"Holes: Outer middle holes are centered in patch (not PCB).")
 
 	# Precalculate the columns
 
@@ -355,7 +374,6 @@ with script:
 	# Make the top and bottom of the antenna PCB are outline_padding away from the top and bottom screws
 	# Make the left and right of the antenna PCB the outer edge of the cutting guide, so we don't waste PCB material
 	# Padding from screw heads to outer edge should be > 1 and < 3 head diameters (according to Tim), so make it 1.5.
-
 	outline_padding = d_countersink*1.5
 	x_outline = x_cutting_guide + 0.001/2 - 0.0002/2
 	top_outline = top_row + outline_padding
@@ -365,13 +383,31 @@ with script:
 	eagle_wire(0.0002, -x_outline, top_outline, -x_outline, bottom_outline)
 	eagle_wire(0.0002, -x_outline, bottom_outline, x_outline, bottom_outline)
 	eagle_wire(0.0002,  x_outline, top_outline,  x_outline, bottom_outline)
-
+	
+	# Calculate final widths and lengths
 	final_width = x_outline * 2
  	final_length = top_outline - bottom_outline 
-	print('Final dimensions of the antenna are ' + str(final_width*1000) + ' x ' + str(final_length*1000) + ' mm (' + str(final_width/0.0254) + ' x ' + str(final_length/0.0254) + ' in)')
+	dprint(1,'Final dimensions of the antenna are ' + str(final_width*1000) + ' x ' + str(final_length*1000) + ' mm (' + str(final_width/0.0254) + ' x ' + str(final_length/0.0254) + ' in)')
+
+	# Put everything into positive coordinates. Probably not necessary, but we've always done this so yay for fuzzy bunnies.
+	script.write('group all;\n')
+	script.write('move (> 0 0) (' + str(final_width/2*1000) + ' ' + str(-bottom_outline*1000) + ');\n')
+
+	# If we can fit multiple antennas in under 11 inches, please do.
+	script.write('group all;\n')
+	script.write('cut (0 0);\n')
+	y_copy = 0
+	dprint(1,"There are " + str(int(11 * 0.0254 / final_length)) + " antennas on this PCB.")
+	for i in range(int(11 * 0.0254 / final_length)-1):
+		y_copy += final_length
+		script.write('paste (0 ' + str(y_copy*1000) + ');\n')
+
+	# we're done here. Fit the hole thing in the windo, and exit
 	script.write('window fit;\n')
 
 # -------------------------------------------------------------------------------------------------------------
 # Script finish
 
-subprocess.Popen(["eagle", "-C", "script ./" + filename, "test.brd"])
+if launch_eagle == True:
+	subprocess.Popen(["eagle", "-C", "script ./" + filename, "test.brd"])
+
